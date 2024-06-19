92ebb0520c6f2e1bfb35791a6cbfbdfd6ffe1611
Meer Kamleshkumar Patel <mr418607@dal.ca>
2024-06-19 17:38:43 -0300

    added home component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RegisterUser from './components/RegisterUser';
 import LoginUser from './components/LoginUser';
 import TwoFAForm from './components/TwoFAForm';
+import Home from './components/Home';
 function App() {
   return (
     <div >
       <Router>
         <Routes>
           <Route path="/register" element={<RegisterUser />} />
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<LoginUser />} />
           <Route path="/twofa" element={<TwoFAForm />} />
         </Routes>
