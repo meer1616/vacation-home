51f6ad82b772f5101ed3379793ef8913816ad57d
Meer Kamleshkumar Patel <mr418607@dal.ca>
2024-06-18 23:12:31 -0300

    starter template

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TextField from '@mui/material/TextField';
+function App() {
+  return (
+    <div className="App">
+      <br />
+      <span className='text-red-600'>hi there</span>
+      <br />
+      <TextField id="outlined-basic" label="Full name" variant="outlined" />
+    </div>
+  );
+}
+
+export default App;
